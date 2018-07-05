54631c312f4f4cf3ee94004e71753ff1d93df068
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-07-05 13:22:40 -0400

    nextBoard

--- game/index.js
+++ game/index.js
@@ -1,6 +1,35 @@
-import {Map} from 'immutable'
-
-export default function reducer(state, action) {
-  // TODO
-  return state
+import { Map } from "immutable";
+//let board = Map();
+//board = board.setIn([1, 1], "X");
+//ACTION
+export const MOVE = "MOVE";
+//ACTION CREATER
+export const move = (player, coord) => {
+  type: MOVE;
+  player, coord;
+};
+function turnReducer(currentTurn = "X", action) {
+  if (action.type === MOVE) return currentTurn === "X" ? "O" : "X";
+  return currentTurn;
 }
+function board(board = Map(), { type, coord, player }) {
+  if (type === MOVE) return board.setIn(coord, player);
+  return board;
+}
+
+export default function reducer(state = {}, action) {
+  const nextBoard = board(state.board, action);
+  return {
+    turn: turnReducer(state.turn, action),
+    board: nextBoard
+  };
+}
+// export default function reducer(state = {}, action) {
+//   // TODO
+//   switch (action.type) {
+//     case MOVE:
+//       return { board: newBoard, action };
+//     default:
+//       return state;
+//   }
+// }
